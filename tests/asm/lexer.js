f3584fe5620168ea522985d5d8cd8b24e2b6ac35
Ben Wightman <benwightman@earthlink.net>
2022-10-30 12:01:22 -0500

    updates to tests

--- tests/asm/lexer.js
+++ tests/asm/lexer.js
@@ -1,15 +1,10 @@
-const Lexer = require('../../asm/parser/lexer.js');
-const assert = require('assert');
 const fs = require('fs');
-const {TokenType} = require('../../asm/parser/token');
-const Parser = require('../../asm/parser/parser.js');
 const {assemble} = require('../../asm');
 
 const source = fs.readFileSync(
-  require.resolve('./test1.txt'), 
+  require.resolve('./test2.txt'), 
   {encoding: 'utf-8'}
 );
-// console.log(source);
 
 console.log(assemble(source, 'test'));
 debugger;
